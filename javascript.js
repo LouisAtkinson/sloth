56cf314c411e6dde97a4357324f2a5e0c0bbc80d
Louis Atkinson <119425566+LouisAtkinson@users.noreply.github.com>
2023-01-22 12:09:59 +0000

    Centralise heading

--- javascript.js
+++ javascript.js
@@ -3,7 +3,6 @@ sloth.classList.add("sloth");
 let height = 50;
 
 document.body.addEventListener("click", function() {
-/*     document.getElementById("heading").style.display = "none"; */
     height += 50;
     sloth.style.height = height + "px";
 });
